effcb92d8a7b63cb6c7826b3cfd5b7ec61953f3f
Guilherme <guilherme.montnezes@gmail.com>
2022-09-22 10:33:13 -0300

    Implementação base

---
+++ src/main/kotlin/br/com/sisvoli/controllers/Hello.kt
@@ -0,0 +1,15 @@
+package br.com.sisvoli.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/alo")
+class Hello {
+
+    @GetMapping
+    fun oi(): String {
+        return "oi"
+    }
+}
