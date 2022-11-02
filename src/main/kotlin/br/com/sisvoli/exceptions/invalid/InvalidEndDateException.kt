a1c5532235f41d69313feec3352ae73069b5da91
Brayan157 <103609696+Brayan157@users.noreply.github.com>
2022-11-01 21:34:14 -0300

    Salvando e validando opções de enquete

--- src/main/kotlin/br/com/sisvoli/exceptions/invalid/InvalidEndDateException.kt
+++ src/main/kotlin/br/com/sisvoli/exceptions/invalid/InvalidEndDateException.kt
@@ -3,5 +3,6 @@ package br.com.sisvoli.exceptions.invalid
 import br.com.sisvoli.exceptions.SisvoliBaseRuntimeException
 
 class InvalidEndDateException(
-    override val message: String = "A Data de finalização da enquete precisa ser maior que a data de inicialização da enquete"
+    override val message: String = "A Data de finalização da enquete precisa ser" +
+        " maior que a data de inicialização da enquete"
 ) : SisvoliBaseRuntimeException(message)
