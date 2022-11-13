d59b055ecf6c17bea58afbb5e7691605d5c083c3
Guilherme <guilherme.montnezes@gmail.com>
2022-11-13 11:14:31 -0300

    "Logging everywhere"

--- src/main/kotlin/br/com/sisvoli/database/repositories/implementations/UserRepositoryImpl.kt
+++ src/main/kotlin/br/com/sisvoli/database/repositories/implementations/UserRepositoryImpl.kt
@@ -9,6 +9,7 @@ import br.com.sisvoli.enums.RoleEnum
 import br.com.sisvoli.exceptions.notFound.UserNotFoundException
 import br.com.sisvoli.models.PasswordRecoveryTokenModel
 import br.com.sisvoli.models.UserModel
+import mu.KotlinLogging
 import org.springframework.stereotype.Component
 import java.util.UUID
 
@@ -19,35 +20,47 @@ class UserRepositoryImpl(
     private val passwordRecoveryTokenSpringDataRepository: PasswordRecoveryTokenSpringDataRepository
 ) : UserRepository {
     override fun save(userModel: UserModel): UserModel {
+        logger.info { "Starting to save user with document ${userModel.cpf}..." }
         val userEntity = UserEntity.of(userModel, roleSpringDataRepository.findByName(RoleEnum.DEFAULT.name))
         return userSpringDataRepository.save(userEntity).toUserModel()
     }
 
     override fun findById(id: UUID): UserModel {
+        logger.info { "Starting to find user by id: $id..." }
         return userSpringDataRepository.findById(id).orElseThrow { UserNotFoundException() }.toUserModel()
     }
 
     override fun findByUsername(username: String): UserModel {
+        logger.info { "Starting to find user by username: $username..." }
         return userSpringDataRepository.findByUsername(username)?.toUserModel() ?: throw UserNotFoundException()
     }
 
     override fun existsByEmail(email: String): Boolean {
+        logger.info { "Starting to check if user with email $email exists..." }
         return userSpringDataRepository.existsByEmail(email)
     }
 
     override fun findByCpf(cpf: String): UserModel {
+        logger.info { "Starting to find user by cpf: $cpf..." }
         return userSpringDataRepository.findByCpf(cpf)?.toUserModel() ?: throw UserNotFoundException()
     }
 
     override fun findRecoverTokenByUserId(userId: UUID): PasswordRecoveryTokenModel? {
+        logger.info { "Starting to find recovery token by userId: $userId..." }
         return passwordRecoveryTokenSpringDataRepository.findByUserEntityId(userId)?.toPasswordRecoveryTokenModel()
     }
 
     override fun existsByCpf(cpf: String): Boolean {
+        logger.info { "Starting to check if user with document $cpf exists..." }
         return userSpringDataRepository.existsByCpf(cpf)
     }
 
     override fun existsByUsername(username: String): Boolean {
+        logger.info { "Starting to check if user with username $username exists..." }
         return userSpringDataRepository.existsByUsername(username)
+    }
+
+    companion object {
+        private val logger = KotlinLogging.logger { }
     }
 }
