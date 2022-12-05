ba71f18b6f7a010bf77680a7363328b2a9c3cadf
Guilherme <guilherme.montnezes@gmail.com>
2022-12-05 13:33:19 -0300

    "Put address"

---
+++ src/main/kotlin/br/com/sisvoli/api/controllers/CityController.kt
@@ -0,0 +1,20 @@
+package br.com.sisvoli.api.controllers
+
+import br.com.sisvoli.models.CityModel
+import br.com.sisvoli.services.interfaces.CityService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/city")
+class CityController(
+    val cityService: CityService
+) {
+
+    @GetMapping("/{cityId}")
+    fun findById(@PathVariable cityId: Long): CityModel {
+        return cityService.findById(cityId)
+    }
+}
