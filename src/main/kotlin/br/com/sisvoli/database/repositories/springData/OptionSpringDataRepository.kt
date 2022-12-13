6d8cdd8f378049ffd3676a77ef65a544bb1b3185
Guilherme <guilherme.montnezes@gmail.com>
2022-12-12 23:44:16 -0300

    Recebendo lista de opções ao salvar novo arquivo

--- src/main/kotlin/br/com/sisvoli/database/repositories/springData/OptionSpringDataRepository.kt
+++ src/main/kotlin/br/com/sisvoli/database/repositories/springData/OptionSpringDataRepository.kt
@@ -3,9 +3,6 @@ package br.com.sisvoli.database.repositories.springData
 import br.com.sisvoli.database.entities.OptionEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
-import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
-import org.springframework.transaction.annotation.Transactional
 
 interface OptionSpringDataRepository : JpaRepository<OptionEntity, UUID> {
     fun deleteAllByPollEntityId(pollId: UUID)
