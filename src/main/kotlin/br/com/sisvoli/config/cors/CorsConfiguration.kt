db8c7474a455fb0d77d51bbe7db1fc5514f778d4
Guilherme <guilherme.montnezes@gmail.com>
2022-12-14 13:33:50 -0300

    Cors

--- src/main/kotlin/br/com/sisvoli/config/cors/CorsConfiguration.kt
+++ src/main/kotlin/br/com/sisvoli/config/cors/CorsConfiguration.kt
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class CorsConfiguration: WebMvcConfigurer {
+class CorsConfiguration : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
